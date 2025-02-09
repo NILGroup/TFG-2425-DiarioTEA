53c9c23380b251fea2b9eb6a33ccb2ee82ea2962
vicmn9 <victoriabasket10@gmail.com>
2025-02-09 20:46:05 +0100

    avances añadir registro al diario

--- Diseño/js/addRegistros.js
+++ Diseño/js/addRegistros.js
@@ -16,14 +16,14 @@ $(document).ready(function () {
         // Obtener la fecha actual en formato ISO (AAAA-MM-DD)
         var fecha = new Date().toISOString().split('T')[0];
         // Obtener la hora actual en formato de 24 horas (HH:MM)
-        var hora = new Date().toLocaleTimeString('it-IT', { hour: '2-digit', minute:'2-digit' });
+        var hora = new Date().toLocaleTimeString('it-IT', { hour: '2-digit', minute: '2-digit' });
 
         // Asignar la fecha y hora a los campos del formulario
         $('#fecha').val(fecha);
         $('#hora').val(hora);
     });
 
-    $("#asociarRutina").on('change', function(){
+    $("#asociarRutina").on('change', function () {
         if ($('#asociarRutina').is(':checked')) {
             $('#selectRutinaContainer').append(`
                 <div class="form-group mt-3" id="selectRutinaGroup">
@@ -39,11 +39,47 @@ $(document).ready(function () {
         }
     })
 
-    $("#buttonDeleteRegistro").on('click', function(){
-        $("#columnaGuardarDiario").hide();
-        $("#columnaFormDiario").hide();
-        $("#columnaAddDiario").show();
-        $("#columnaVistaDiario").show();
-    })
+    
+
+    $("#columnaFormDiario .card").each(function(index) {
+        $(this).attr("data-index", index); // Guarda el índice original
+        $(this).data("originalContainer", $(this).parent()); // Guarda el contenedor original
+    });
+
+    $(".container.d-flex").on("click", ".card", function() {
+        let card = $(this);
+        let contenedorPictogramas = $("#contenedorPictogramas");
+
+        if (card.parent().is(contenedorPictogramas)) {
+            // Volver al contenedor original y restaurar el orden
+            let originalContainer = card.data("originalContainer");
+            let index = card.attr("data-index");
+
+            let inserted = false;
+            originalContainer.children(".card").each(function() {
+                if (parseInt($(this).attr("data-index")) > index) {
+                    $(this).before(card);
+                    inserted = true;
+                    return false; // Salir del bucle
+                }
+            });
+
+            // Si no se insertó antes de otro, agregar al final
+            if (!inserted) {
+                originalContainer.append(card);
+            }
+            card.find(".cross-icon").remove();
+
+        } else {
+            // Mover al contenedor de pictogramas
+            contenedorPictogramas.append(card);
+            card.append('<span class="cross-icon">✖</span>');
+            
+        }
+    });
+
+
+
+
 });
 
