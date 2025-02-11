755d4ea69895fd94b3fc7b92584deff07913cde2
vicmn9 <124901747+vicmn9@users.noreply.github.com>
2025-02-11 20:59:19 +0100

    comienzo de configurar tarjetas elegibles

---
+++ Diseño/js/manageCardsTablero.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+
+
+
+    $("#columnaFormDiario .card").each(function (index) {
+        $(this).attr("data-index", index); // Guarda el índice original
+        $(this).data("originalContainer", $(this).parent()); // Guarda el contenedor original
+    });
+
+    $(".container.d-flex").on("click", ".card", function () {
+        let card = $(this);
+        let contenedorPictogramas = $("#rowPictogramas");
+
+        if (card.parent().is(contenedorPictogramas)) {
+            // Volver al contenedor original y restaurar el orden
+            let originalContainer = card.data("originalContainer");
+            let index = card.attr("data-index");
+
+            let inserted = false;
+            originalContainer.children(".card").each(function () {
+                if (parseInt($(this).attr("data-index")) > index) {
+                    $(this).before(card);
+                    inserted = true;
+                    return false; // Salir del bucle
+                }
+            });
+
+            // Si no se insertó antes de otro, agregar al final
+            if (!inserted) {
+                originalContainer.append(card);
+            }
+            card.find(".cross-icon").remove();
+
+        } else {
+            // Mover al contenedor de pictogramas
+            contenedorPictogramas.append(card);
+            card.append('<span class="cross-icon">✖</span>');
+
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+})
