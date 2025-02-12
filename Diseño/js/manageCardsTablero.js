1770da0fb678ef0cc42e3a1d8d850a6f5eeb6fb4
jbroto <jbroto@ucm.es>
2025-02-12 19:48:13 +0100

    Update manageCardsTablero.js

--- Diseño/js/manageCardsTablero.js
+++ Diseño/js/manageCardsTablero.js
@@ -10,19 +10,16 @@ $(document).ready(function () {
     $(".row").on("click", ".card", function (event) {
         event.preventDefault();
     
-        let cardContainer = $(this).closest(".col-lg-2, .col-md-3"); // Contenedor de la tarjeta
+        let cardContainer = $(this).closest(".col-lg-2, .col-md-3");
         let contenedorPictogramas = $("#contenedorPictogramas");
-        let rowSuperior = $("#columnaFormDiario .row.mt-3"); // Fila superior donde deben ir
+        let rowPictos = $("#columnaFormDiario .row.mt-3");
     
-        // Si la tarjeta ya está en contenedorPictogramas, moverla de vuelta a la fila superior
         if (cardContainer.parent().is(contenedorPictogramas)) {
             let originalContainer = cardContainer.data("originalContainer");
-    
             if (originalContainer && originalContainer.length) {
                 let inserted = false;
                 let index = parseInt(cardContainer.attr("data-index"));
-    
-                // Intentar insertarlo en su posición original
+
                 originalContainer.children(".col-lg-2, .col-md-3").each(function () {
                     if (parseInt($(this).attr("data-index")) > index) {
                         $(this).before(cardContainer);
@@ -30,25 +27,18 @@ $(document).ready(function () {
                         return false;
                     }
                 });
-    
                 if (!inserted) {
                     originalContainer.append(cardContainer);
                 }
             } else {
-                // Si no tiene un contenedor original, llevarlo a la fila superior
-                rowSuperior.append(cardContainer);
+                rowPictos.append(cardContainer);
             }
-    
-            cardContainer.find(".cross-icon").remove(); // Quitar la cruz
-    
+            cardContainer.find(".cross-icon").remove(); 
         } else {
-            // Guardar el contenedor original solo si no se ha guardado antes
             if (!cardContainer.data("originalContainer")) {
                 cardContainer.data("originalContainer", cardContainer.parent());
                 cardContainer.attr("data-index", cardContainer.index());
             }
-    
-            // Mover al contenedor de pictogramas
             cardContainer.append('<span class="cross-icon">&times</span>'); 
             contenedorPictogramas.append(cardContainer);
         }
