723e4c8275bbc3676e2b70209ca2c0f887c87d69
Victoria Martín <victoria.martin@axonpartnersgroup.com>
2025-02-17 08:01:56 +0100

    cambio visual iniciar rutina

--- Diseño/js/iniciarRutina.js
+++ Diseño/js/iniciarRutina.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
 
 
-    $('#buttonIniciar').click(function() {
+    $('#buttonIniciar').click(function () {
         // Selecciona la primera tarjeta dentro del contenedorRutina
         var primeraTarjeta = $('#contenedorRutina .col-lg-2').first();
+        
 
-        // Verifica si hay una tarjeta para mover
+
+
         if (primeraTarjeta.length > 0) {
-            // Mueve la tarjeta al contenedorRutinaHecha
+            var imagenTarjeta = primeraTarjeta.find('.card-img').attr('src');
+            $('#buttonIniciar .card-img').attr('src', imagenTarjeta);
             $('#contenedorRutinaHecha .row').append(primeraTarjeta);
+            
+        }
+        else{
+            $('#buttonIniciar .card-img').attr('src', "images/config/iniciar.png");
         }
     });
 
