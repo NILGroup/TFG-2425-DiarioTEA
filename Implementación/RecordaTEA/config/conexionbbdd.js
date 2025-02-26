7abf53f8bf0358183bef4f6c87f86c83649eba5e
jbroto <jbroto@ucm.es>
2025-02-26 19:59:57 +0100

    Cambio a mysql2

--- Implementación/RecordaTEA/config/conexionbbdd.js
+++ Implementación/RecordaTEA/config/conexionbbdd.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 
 //Creamos un pool de conexiones
 const pool = mysql.createPool({
@@ -7,7 +7,9 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10
 });
 
 module.exports = pool;
