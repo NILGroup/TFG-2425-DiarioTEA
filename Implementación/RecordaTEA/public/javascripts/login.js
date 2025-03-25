06daccd140bc1f612f3bbe6febb8f2e95a9f3278
jbroto <jbroto@ucm.es>
2025-03-25 13:33:42 +0100

    Comprobación campos
    ya se comprueba que los campos no estén vacíos a la hora de enviar el formulario

--- Implementación/RecordaTEA/public/javascripts/login.js
+++ Implementación/RecordaTEA/public/javascripts/login.js
@@ -39,33 +39,87 @@ $(document).ready(function () {
             let confirm = $('#passw-confirmacion').val();
 
             if (pass === confirm) {
-                $("#error").hide();
                 $("#acceder").prop("disabled", false);
+                $('#passw-confirmacion').removeClass('is-invalid');
             }
             else {
                 $("#acceder").prop("disabled", true);
-                $("#error").show();
+                $("#error").text('Las contraseñas no coinciden');
+                $('##passw-confirmacion').addClass('is-invalid');
             }
         }
     });
 
-    $('#login').on('submit', function(e){
+    $('#login').on('submit', function (e) {
         e.preventDefault();
         let usuario = $('#usuario');
         let passw = $('#passw');
+        let confirm = $('#passw-confirmacion');
+        let nombre = $('#nombre');
+        let rol = $('#rol');
+        let valido = true;
 
         let usuarioValor = usuario.val().trim();
-        let passwValor = $('#passw').val().trim();
+        let passwValor = passw.val().trim();
+        let confirmValor = confirm.val().trim();
+        let nombreValor = nombre.val().trim();
+        let rolValor = rol.val();
 
         usuario.removeClass('is-invalid');
+        passw.removeClass('is-invalid');
+        confirm.removeClass('is-invalid');
+        nombre.removeClass('is-invalid');
+        rol.removeClass('is-invalid');
+
 
         if (usuarioValor === '') {
             usuario.addClass('is-invalid');
+            valido = false;
+        }
+        if (passwValor === '') {
+            passw.addClass('is-invalid');
+            valido = false;
         }
 
-        $.ajax({
-            type: 'POST',
-            url: '/inicio',
-        });
+        if ($('#tit-pag').text() === 'Crear cuenta') {
+            if (confirmValor === '') {
+                confirm.addClass('is-invalid');
+                $('#error').text('Campo obligatorio.')
+                valido = false;
+            }
+            if (nombreValor === '') {
+                nombre.addClass('is-invalid');
+                valido = false;
+            }
+            if (rolValor === 'Seleccione una opción' || !rolValor) {
+                rol.addClass('is-invalid');
+                valido = false;
+            }
+        }
+
+        if (valido) {
+            if ($('#tit-pag').text() === 'Crear cuenta') {
+
+            } else {
+                let u = {
+                    usuario: usuarioValor,
+                    passw: passwValor
+                };
+
+                $.ajax({
+                    url: '/users/login',
+                    method: 'POST',
+                    data: {
+                        usuario: u
+                    },
+                    success: function(){
+                        
+                    },
+                    error: function(){
+
+                    }
+                });
+            }
+        }
     })
 });
