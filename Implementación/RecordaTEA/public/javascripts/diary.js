f040f0de754ef97320e38184f3b44e7b86632381
jbroto <jbroto@ucm.es>
2025-04-01 14:00:41 +0200

    Cambio script.
    Se ha movido el script del html al script diary.js.

--- Implementación/RecordaTEA/public/javascripts/diary.js
+++ Implementación/RecordaTEA/public/javascripts/diary.js
@@ -1,4 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+
+    const currentYear = new Date().getFullYear();
+    const startYear = '2000';
+
+    $('#anio').datepicker({
+        format: "yyyy", 
+        startView: "years",  
+        minViewMode: "years",  
+        startDate: startYear, 
+        endDate: currentYear + '-12-31',
+        autoclose: true
+    }).datepicker("setDate", currentYear + "-01-01");
+
+    const currentMonth = new Date().getMonth() + 1;  
+    $('#mes').val(currentMonth);
 
     ajustarAlturas();
 
@@ -6,4 +22,5 @@ $(document).ready(function(){
         let maxHeight = Math.max($(".list-dia").outerHeight(), $(".list-contenido").outerHeight());
         $(".list-dia, .list-contenido").height(maxHeight);
     }
+
 });
