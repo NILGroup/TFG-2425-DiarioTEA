230271904896c8a1329f01d0a5cff9169036bda0
jbroto <jbroto@ucm.es>
2025-04-08 11:23:13 +0200

    Update entradasControlador.js
    Añadidos control de acceso y de errores en los métodos del controlador de entradas.

--- Implementación/RecordaTEA/controladores/entradasControlador.js
+++ Implementación/RecordaTEA/controladores/entradasControlador.js
@@ -12,200 +12,240 @@ class EntradasController {
 
     async cargarDiario(req, res) {
         try {
-            let usuario;
-            let usus = req.session.usuarios;
-            let con;
-            if (!req.session.usuario || req.session.usuario.id !== req.params.id) {
-                const resultado = await usuariosService.leerUsuarioId(req.params.id);
-                const c = await tarjetasService.leerConfiguracionVocabulario(resultado[0].id);
-                usuario = resultado[0];
-                con = c;
-                req.session.usuario = resultado[0];
-                req.session.config = c;
-            }
-            else {
-                usuario = req.session.usuario;
-                con = req.session.config;
-            }
-            let fecha = new Date();
-            let año = fecha.getFullYear(); 
-            let mes = fecha.getMonth() + 1; 
-    
-            console.log(mes, año)
-
-            let entradas = await entradasService.entradasMes(usuario.id, mes, año);
-            for (let entrada of entradas) {
-                entrada.mes = fechaUtils.mesAbreviatura(entrada.fecha).toUpperCase();
-                entrada.dia = fechaUtils.dia(entrada.fecha);
-                entrada.hoy = fechaUtils.esHoy(entrada.fecha);
-                entrada.f = fechaUtils.fechaCompleta(entrada.fecha);
-                entrada.fecha = fechaUtils.fechaSinHora(entrada.fecha);
-                if (entrada.cuerpo === null) {
-                    let pictos = await tarjetasService.tarjetasEntrada(entrada.id);
-                    entrada.pictos = pictos;
+            if (req.session.logged && req.session.cuidador) {
+                let usuario;
+                let usus = req.session.usuarios;
+                let con;
+                if (!req.session.usuario || req.session.usuario.id !== req.params.id) {
+                    const resultado = await usuariosService.leerUsuarioId(req.params.id);
+                    const c = await tarjetasService.leerConfiguracionVocabulario(resultado[0].id);
+                    usuario = resultado[0];
+                    con = c;
+                    req.session.usuario = resultado[0];
+                    req.session.config = c;
                 }
                 else {
-                    entrada.pictos = [];
+                    usuario = req.session.usuario;
+                    con = req.session.config;
                 }
-            }
+                let fecha = new Date();
+                let año = fecha.getFullYear();
+                let mes = fecha.getMonth() + 1;
 
-            let anyos = await entradasService.obtenerAnyos(usuario.id);
-            res.render('cuidadores/diario', { data: { entradas: entradas, usuario: usuario, usuarios: usus, anyos: anyos } });
+                console.log(mes, año)
+
+                let entradas = await entradasService.entradasMes(usuario.id, mes, año);
+                for (let entrada of entradas) {
+                    entrada.mes = fechaUtils.mesAbreviatura(entrada.fecha).toUpperCase();
+                    entrada.dia = fechaUtils.dia(entrada.fecha);
+                    entrada.hoy = fechaUtils.esHoy(entrada.fecha);
+                    entrada.f = fechaUtils.fechaCompleta(entrada.fecha);
+                    entrada.fecha = fechaUtils.fechaSinHora(entrada.fecha);
+                    if (entrada.cuerpo === null) {
+                        let pictos = await tarjetasService.tarjetasEntrada(entrada.id);
+                        entrada.pictos = pictos;
+                    }
+                    else {
+                        entrada.pictos = [];
+                    }
+                }
+
+                let anyos = await entradasService.obtenerAnyos(usuario.id);
+                res.status(200).render('cuidadores/diario', { data: { entradas: entradas, usuario: usuario, usuarios: usus, anyos: anyos } });
+            }
+            else {
+                res.redirect('/');
+            }
         }
         catch (error) {
-            throw error;
+            res.status(500).render('error');
         }
     }
 
     async cargarDia(req, res) {
         try {
-            let dia = req.params.dia;
-            let usuario = req.session.usuario;
-            let usus = req.session.usuarios;
-            let entradas = await entradasService.entradasDia(dia, usuario.id);
-            for (let entrada of entradas) {
-                let autor = await usuariosService.leerUsuarioId(entrada.autor);
-                entrada.autor = autor[0];
-                entrada.hora = fechaUtils.horaMinutos(entrada.fecha_registro);
-                if (entrada.cuerpo === null) {
-                    let pictos = await tarjetasService.tarjetasEntrada(entrada.id);
-                    entrada.pictos = pictos;
+            if (req.session.logged && req.session.cuidador) {
+                let dia = req.params.dia;
+                let usuario = req.session.usuario;
+                let usus = req.session.usuarios;
+                let entradas = await entradasService.entradasDia(dia, usuario.id);
+                for (let entrada of entradas) {
+                    let autor = await usuariosService.leerUsuarioId(entrada.autor);
+                    entrada.autor = autor[0];
+                    entrada.hora = fechaUtils.horaMinutos(entrada.fecha_registro);
+                    if (entrada.cuerpo === null) {
+                        let pictos = await tarjetasService.tarjetasEntrada(entrada.id);
+                        entrada.pictos = pictos;
+                    }
+                    else {
+                        entrada.pictos = [];
+                    }
                 }
-                else {
-                    entrada.pictos = [];
-                }
+                let diaCompleto = fechaUtils.fechaCompleta(dia);
+                res.render('cuidadores/verDia', { data: { entradas: entradas, usuario: usuario, dia: diaCompleto, usuarios: usus, config: req.session.config } });
             }
-            let diaCompleto = fechaUtils.fechaCompleta(dia);
-            res.render('cuidadores/verDia', { data: { entradas: entradas, usuario: usuario, dia: diaCompleto, usuarios: usus, config: req.session.config } });
+            else {
+                res.redirect('/');
+            }
         }
         catch (error) {
-            throw error;
+            res.status(500).render('error');
         }
     }
 
-    async actualizarDiario(req, res){
-        let entradas = await entradasService.entradasMes(req.session.usuario.id, req.query.mes, req.query.anyo);
-        for (let entrada of entradas) {
-            entrada.mes = fechaUtils.mesAbreviatura(entrada.fecha).toUpperCase();
-            entrada.dia = fechaUtils.dia(entrada.fecha);
-            entrada.hoy = fechaUtils.esHoy(entrada.fecha);
-            entrada.f = fechaUtils.fechaCompleta(entrada.fecha);
-            entrada.fecha = fechaUtils.fechaSinHora(entrada.fecha);
-            if (entrada.cuerpo === null) {
-                let pictos = await tarjetasService.tarjetasEntrada(entrada.id);
-                entrada.pictos = pictos;
+    async actualizarDiario(req, res) {
+        try {
+            if (req.session.logged) {
+                let entradas = await entradasService.entradasMes(req.session.usuario.id, req.query.mes, req.query.anyo);
+                for (let entrada of entradas) {
+                    entrada.mes = fechaUtils.mesAbreviatura(entrada.fecha).toUpperCase();
+                    entrada.dia = fechaUtils.dia(entrada.fecha);
+                    entrada.hoy = fechaUtils.esHoy(entrada.fecha);
+                    entrada.f = fechaUtils.fechaCompleta(entrada.fecha);
+                    entrada.fecha = fechaUtils.fechaSinHora(entrada.fecha);
+                    if (entrada.cuerpo === null) {
+                        let pictos = await tarjetasService.tarjetasEntrada(entrada.id);
+                        entrada.pictos = pictos;
+                    }
+                    else {
+                        entrada.pictos = [];
+                    }
+                }
+                res.status(200).send({ data: entradas });
             }
-            else {
-                entrada.pictos = [];
-            }
+            else { res.redirect('/'); }
         }
-        console.log(entradas);
-        res.status(200).send({data: entradas});
+        catch (error) {
+            res.status(500).send('Ha ocurrido algo en el servidor');
+        }
     }
 
     async redirectToDiary(req, res) {
-        const response = await entradasService.leerEntradasPorUsuario(req.session.usuario.id);
-        res.render('TEA/diaryTEA', { entradas: response, diary: true, usuario: req.session.usuario.nombre, config: req.session.config });
+        try {
+            if (req.session.logged && !req.session.cuidador) {
+                const response = await entradasService.leerEntradasPorUsuario(req.session.usuario.id);
+                res.status(200).render('TEA/diaryTEA', { entradas: response, diary: true, usuario: req.session.usuario.nombre, config: req.session.config });
+            }
+            else {
+                res.redirect('/');
+            }
+        }
+        catch (error) {
+            res.status(500).render('error');
+        }
     }
 
     async addEntry(req, res) {
-
-        // Significa que no tiene tarjetas
-        var vocabulario =[];
-        if(!req.session.config.texto){
-            vocabulario = await tarjetasService.obtenerTarjetasUsuarioId(req.session.usuario.id);
+        try {
+            if (req.session.logged && !req.session.cuidador) {
+                // Significa que no tiene tarjetas
+                var vocabulario = [];
+                if (!req.session.config.texto) {
+                    vocabulario = await tarjetasService.obtenerTarjetasUsuarioId(req.session.usuario.id);
+                }
+                res.status(200).render('TEA/addEntryTEA', { vocabulario: vocabulario, diary: true, usuario: req.session.usuario.nombre, config: req.session.config })
+                //OBTENER PICTOGRAMAS DEL USUARIO PARA ADD ENTRY Y RENDERIZAR
+            }
+            else {
+                res.redirect('/');
+            }
         }
-
-        console.log(vocabulario);
-        res.render('TEA/addEntryTEA', {vocabulario: vocabulario, diary: true, usuario: req.session.usuario.nombre, config: req.session.config})
-
-        //OBTENER PICTOGRAMAS DEL USUARIO PARA ADD ENTRY Y RENDERIZAR
-
+        catch (error) {
+            res.status(500).render('error');
+        }
     }
 
-    async submitEditEntry(req, res){
-        const registrosString = req.body.registros; 
-        const registros = JSON.parse(registrosString); 
-       
-        const response = await entradasService.submitEditEntry(registros);
+    async submitEditEntry(req, res) {
+        try {
+            if (req.session.logged && !req.session.cuidador) {
+                const registrosString = req.body.registros;
+                const registros = JSON.parse(registrosString);
 
-        if(response.success){
-            return res.redirect('/diario'); // Redirigir a la página del diario, por ejemplo
-        }   
-        else{
-            return res.status(500).send('Error interno del servidor');
+                const response = await entradasService.submitEditEntry(registros);
+
+                if (response.success) {
+                    return res.redirect('/diario'); // Redirigir a la página del diario, por ejemplo
+                }
+                else {
+                    return res.status(500).send('Error interno del servidor');
+                }
+            }
+            else { res.redirect('/') }
         }
-
+        catch (error) {
+            res.status(500).render('error');
+        }
     }
 
     async viewEntry(req, res) {
+        try {
+            if (req.session.logged && !req.session.cuidador) {
+                const idEntrada = req.params.idEntrada;
+                const idUsuario = req.session.usuario.id; // Asumiendo que tienes la información del usuario autenticado
+                // si no se encuentra en la sesión, realiza la búsqueda en la base de datos
+                const entrada = await entradasService.viewEntryById(idEntrada, idUsuario);
+                if (entrada) {
+                    return res.status(200).render('TEA/viewEntryTEA', { entrada: entrada[0], diary: true, usuario: req.session.usuario.nombre, config: req.session.config });
+                } else {
+                    const error = {
+                        status: 403,
+                        info: "No tienes permisos para ver esta página"
+                    };
+                    // si no existe, es porque no ha cumplido con que sea el idUsuario
+                    return res.status(403).render('error', { error: error });
+                }
+            }
+            else {
+                res.redirect('/');
+            }
+        } catch (error) {
+            return res.status(500).send('Error interno del servidor');
+        }
+    }
+
+    async submitEntrada(req, res) {
+        try {
+            const registrosString = req.body.registros;
+            const registros = JSON.parse(registrosString);
+            registros.id_usuario = req.session.usuario.id;
+
+            const response = await entradasService.submitEntrada(registros);
+            console.log(response);
+            if (response.success) {
+                res.redirect('/diario'); // Redirigir a la página del diario, por ejemplo
+            }
+        }
+        catch(error){
+            res.status(500).render('error');
+        }
+    }
+
+    async eliminarEntrada(req, res) {
+
         const idEntrada = req.params.idEntrada;
         const idUsuario = req.session.usuario.id; // Asumiendo que tienes la información del usuario autenticado
-
         try {
-    
+
+            const response = await entradasService.eliminarEntrada(idEntrada, idUsuario);
             // si no se encuentra en la sesión, realiza la búsqueda en la base de datos
-            const entrada = await entradasService.viewEntryById(idEntrada, idUsuario);
-            console.log(entrada);
-            if (entrada) {
-                console.log(entrada[0].tarjetas);
-                return res.render('TEA/viewEntryTEA', { entrada: entrada[0],  diary: true, usuario: req.session.usuario.nombre,config: req.session.config  });
+            if (response.success) {
+                res.redirect('/diario'); // Redirigir a la página del diario, por ejemplo
             } else {
                 const error = {
                     status: 403,
                     info: "No tienes permisos para ver esta página"
                 };
                 // si no existe, es porque no ha cumplido con que sea el idUsuario
-                return res.render('error', { error: error });
+                return res.status(403).render('error', { error: error });
             }
         } catch (error) {
-            console.log(error);
             return res.status(500).send('Error interno del servidor');
         }
-    }
 
-    async submitEntrada(req, res){
-
-        const registrosString = req.body.registros; 
-        const registros = JSON.parse(registrosString); 
-        registros.id_usuario = req.session.usuario.id;
-
-        const response = await entradasService.submitEntrada(registros);
-        console.log(response);
-        if(response.success){
-            res.redirect('/diario'); // Redirigir a la página del diario, por ejemplo
-        }   
-        
-    }
-
-    async eliminarEntrada(req, res){
-
-        const idEntrada = req.params.idEntrada;
-        const idUsuario = req.session.usuario.id; // Asumiendo que tienes la información del usuario autenticado
-
-        try {
-    
-            const response = await entradasService.eliminarEntrada(idEntrada, idUsuario);
-            // si no se encuentra en la sesión, realiza la búsqueda en la base de datos
-            if(response.success){
-                res.redirect('/diario'); // Redirigir a la página del diario, por ejemplo
-            }  else {
-                const error = {
-                    status: 403,
-                    info: "No tienes permisos para ver esta página"
-                };
-                // si no existe, es porque no ha cumplido con que sea el idUsuario
-                return res.render('error', { error: error });
-            }
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send('Error interno del servidor');
-        } 
-        
     }
 
 
-    async editEntryView(req, res){
+    async editEntryView(req, res) {
 
         const idEntrada = req.params.idEntrada;
         const idUsuario = req.session.usuario.id;
@@ -213,27 +253,26 @@ class EntradasController {
         try {
 
             // Significa que no tiene tarjetas
-            var vocabulario =[];
-            if(!req.session.config.texto){
+            var vocabulario = [];
+            if (!req.session.config.texto) {
                 vocabulario = await tarjetasService.obtenerTarjetasUsuarioId(idUsuario);
             }
-            
+
             // si no se encuentra en la sesión, realiza la búsqueda en la base de datos
             const entrada = await entradasService.viewEntryById(idEntrada, idUsuario);
-            
+
 
             if (entrada) {
-                return res.render('TEA/editEntryTEA', { entrada: entrada[0], diary: true, vocabulario: vocabulario, usuario: req.session.usuario.nombre, config: req.session.config });
+                return res.status(200).render('TEA/editEntryTEA', { entrada: entrada[0], diary: true, vocabulario: vocabulario, usuario: req.session.usuario.nombre, config: req.session.config });
             } else {
                 const error = {
                     status: 403,
                     info: "No tienes permisos para ver esta página"
                 };
                 // si no existe, es porque no ha cumplido con que sea el idUsuario
-                return res.render('error', { error: error });
+                return res.status(403).render('error', { error: error });
             }
         } catch (error) {
-            console.log(error);
             return res.status(500).send('Error interno del servidor');
         }
 
